e219daf7188e9826276db5a23f8e8d61948936d9
Reynaldo Gil <gilcu2@gmail.com>
2019-07-20 08:24:15 +0100

    showing data

--- kde/data.py
+++ kde/data.py
@@ -1,11 +1,8 @@
 from typing import *
-import gzip
 import pickle
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-
-LearnMatrix = Tuple[np.ndarray, np.ndarray]
 
 
 class Data:
@@ -13,23 +10,32 @@ class Data:
     def __init__(self, name: str, path: str, size: Tuple[int, int]) -> None:
         self.name = name
         self.size = size
-        self.train_x, self.train_y = self.load(path)
+        self.features, self.labels = self.load(path)
 
     @staticmethod
-    def load(path: str) -> LearnMatrix:
+    def load(path: str) -> Tuple[np.ndarray, np.ndarray]:
         file = open(path, 'rb')
         data = pickle.load(file, encoding='bytes')
+        file.close()
         if type(data) == tuple:
-            return data[1]
+            features, labels = data
+            features = features.reshape(28, 28)
+            return features, labels
         else:
-            return data
+            labels = np.asarray(data[b"labels"])
+            features = data[b"data"].reshape(10000, 3, 32, 32)
+            return features, labels
 
     def show(self) -> None:
-        plt.imshow(self.train_x[0].reshape(self.size), cmap=cm.Greys_r)
+        plt.imshow(self.features[0], cmap=cm.Greys_r)
         plt.show()
 
 
 if __name__ == '__main__':
-    path = "../data/mnist_py3k.pkl.gz"
-    data = Data("mnist", path, (28, 28))
+    # path = "../data/mnist_py3k.pkl"
+    # data = Data("mnist", path, (28, 28))
+    # data.show()
+
+    path = "../data/cifar_batch-1.pkl"
+    data = Data("cifar", path, (32, 32))
     data.show()
--- tests/test_Data.py
+++ tests/test_Data.py
@@ -4,10 +4,10 @@ from kde.data import Data
 def test_load_mnist():
     path = "../data/mnist_py3k.pkl"
     data = Data("mnist", path, (28, 28))
-    assert data.train_x.size == 39200000
+    assert data.features.size == 39200000
 
 
 def test_load_cifar():
     path = "../data/cifar_batch-1.pkl"
     data = Data("cifar", path, (32, 32))
-    assert data.train_x.size == 39200000
+    assert data.features.size == 30720000
