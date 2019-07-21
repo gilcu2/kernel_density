5d592e3b9ee44ab4fb6a959215ae8a0d5ba0c0d5
Reynaldo Gil <gilcu2@gmail.com>
2019-07-21 15:13:29 +0100

    beginning normalizing

--- kde/data.py
+++ kde/data.py
@@ -4,38 +4,56 @@ import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
+mnist = 'mnist'
+cifar = 'cifar'
+
+
+def to_grey(image: np.ndarray) -> np.ndarray:
+    sum = image.sum(axis=0)
+    grey = sum / 3
+    return grey
+
+
+def to_normalize(image: np.ndarray) -> np.ndarray:
+    sum = image.sum(axis=0)
+    grey = sum / 255
+    return grey
+
 
 class Data:
 
     def __init__(self, name: str, path: str, size: Tuple[int, int]) -> None:
         self.name = name
         self.size = size
-        self.features, self.labels = self.load(path)
+        self.features, self.labels = self.load(path, name)
 
     @staticmethod
-    def load(path: str) -> Tuple[np.ndarray, np.ndarray]:
+    def load(path: str, name: str) -> Tuple[np.ndarray, np.ndarray]:
         file = open(path, 'rb')
         data = pickle.load(file, encoding='bytes')
         file.close()
-        if type(data) == tuple:
-            features, labels = data
-            features = features.reshape(28, 28)
+        if name == mnist:
+            features, labels = data[0]
+            features = features.reshape(50000, 28, 28)
             return features, labels
-        else:
+        elif name == cifar:
             labels = np.asarray(data[b"labels"])
-            features = data[b"data"].reshape(10000, 3, 32, 32)
+            rgb_array = data[b"data"].reshape(10000, 3, 32, 32)
+            features = np.array([to_grey(image) for image in rgb_array])
+            features = np.array([to_normalize(image) for image in features])
             return features, labels
 
     def show(self) -> None:
+        print("label: ", self.labels[0])
         plt.imshow(self.features[0], cmap=cm.Greys_r)
         plt.show()
 
 
 if __name__ == '__main__':
-    # path = "../data/mnist_py3k.pkl"
-    # data = Data("mnist", path, (28, 28))
-    # data.show()
+    path = "../data/mnist_py3k.pkl"
+    data = Data("mnist", path, (28, 28))
+    data.show()
 
     path = "../data/cifar_batch-1.pkl"
-    data = Data("cifar", path, (32, 32))
+    data = Data(cifar, path, (32, 32))
     data.show()
--- tests/test_Data.py
+++ tests/test_Data.py
@@ -1,4 +1,5 @@
-from kde.data import Data
+from kde.data import *
+import numpy as np
 
 
 def test_load_mnist():
@@ -11,3 +12,18 @@ def test_load_cifar():
     path = "../data/cifar_batch-1.pkl"
     data = Data("cifar", path, (32, 32))
     assert data.features.size == 30720000
+
+
+def test_to_fray_level():
+    rgb = np.arange(12).reshape(3, 2, 2)
+    grey = to_grey(rgb)
+    assert grey.shape == (2, 2)
+    assert grey[0, 0] == 4
+
+
+def test_vectorizer_to_fray_level():
+    rgb_array = np.arange(24).reshape(2, 3, 2, 2)
+    print("\nrgb:\n", rgb_array)
+    grey_array = np.array([to_grey(image) for image in rgb_array])
+    assert grey_array.shape == (2, 2, 2)
+    assert grey_array[0, 0, 0] == 4
