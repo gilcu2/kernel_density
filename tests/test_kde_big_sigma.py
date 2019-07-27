3d6eaf243e8804178b740495bbb62ae1c0d7700c
Reynaldo Gil <gilcu2@gmail.com>
2019-07-27 03:51:55 +0100

    test parallel

--- tests/test_kde_big_sigma.py
+++ tests/test_kde_big_sigma.py
@@ -3,7 +3,6 @@ from time import time
 from kde.kde_big_sigma import *
 
 
-
 def test_logpx():
     x = np.array([0.1, 0.2])
     mat = np.array([
@@ -93,3 +92,87 @@ def test_vecs_norm2():
     expected = np.array([5.0, 10.0])
     print(norms2, expected)
     assert np.linalg.norm(norms2 - expected) < 0.000001
+
+def test_sum_probability_parallel_minimun():
+    train = np.array([
+        [0.1, 0.1],
+        [0.4, 0.1],
+    ])
+    validation = np.array([
+        [0.2, 0.1],
+        # [0.2, 0.7],
+    ])
+
+    sigma = 1.0
+
+    r_sequential = sum_probability(train, validation, sigma)
+    r_parallel = sum_probability_parallel(train, validation, sigma)
+    print('Results', r_sequential, r_parallel)
+    assert abs(r_sequential - r_parallel) < 0.0001
+
+def test_sum_probability_paralle_small_data():
+    train = np.array([
+        [0.1, 0.1],
+        [0.2, 0.2],
+    ])
+    validation = np.array([
+        [0.1, 0.1],
+        [0.2, 0.2],
+    ])
+
+    sigmas = [0.02, 0.05, 0.08, 0.1, 0.2, 0.5, 1.0, 1.5, 2.0]
+
+    for sigma in sigmas:
+        print('Sigma:', sigma)
+
+        r_sequential = sum_probability(train, validation, sigma)
+        r_parallel = sum_probability_parallel(train, validation, sigma)
+        print('Results', r_sequential, r_parallel)
+        assert abs(r_sequential - r_parallel) < 0.00001
+
+def test_sum_probability_parallel_medium():
+    np.random.seed(7)
+
+    k = 100
+    d = 100
+    m = 100
+
+    train = np.random.rand(k, d)
+    validation = np.random.rand(m, d)
+
+    sigmas = [0.3, 0.4, 0.5, 0.6, 0.7]
+
+    for sigma in sigmas:
+        print('Sigma:', sigma)
+
+        r_sequential = sum_probability(train, validation, sigma)
+        r_parallel = sum_probability_parallel(train, validation, sigma)
+        print('Results', r_sequential, r_parallel)
+        assert abs(r_sequential - r_parallel) < 0.00001
+
+def test_sum_probability_parallel_real_data():
+    dir = "../data/"
+
+    train_data = LearningData.from_file(cifar, 'train', dir)
+
+    validation_data = LearningData.from_file(cifar, 'validation', dir)
+    print('Validation:', validation_data.shape)
+
+    k = 1000
+    m = 100
+
+    train = train_data.features[:k]
+    validation = validation_data.features[:m]
+
+    print("Training:", train.shape)
+    print('Validation:', validation.shape)
+
+    sigmas = [0.3, 0.4, 0.5, 0.6, 0.7]
+
+    for sigma in sigmas:
+        print('Sigma:', sigma)
+
+        r_sequential = sum_probability(train, validation, sigma)
+        r_parallel = sum_probability_parallel(train, validation, sigma)
+        print('Results', r_sequential, r_parallel)
+        assert abs(r_sequential - r_parallel) < 0.00001
